365f6313be656d1ef1021fe9b9f0135adf3c8ca8
Eduardo Lucrezia Cardoso <eduwh.lucrezia@gmail.com>
2022-04-24 23:58:39 -0300

    Removing obj

--- KeyWorks/obj/Debug/net6.0/KeyWorks.Api.AssemblyInfo.cs
+++ KeyWorks/obj/Debug/net6.0/KeyWorks.Api.AssemblyInfo.cs
@@ -11,7 +11,13 @@
 using System;
 using System.Reflection;
 
-
+[assembly: System.Reflection.AssemblyCompanyAttribute("KeyWorks.Api")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("KeyWorks.Api")]
+[assembly: System.Reflection.AssemblyTitleAttribute("KeyWorks.Api")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
